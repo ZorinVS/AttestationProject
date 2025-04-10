b0b84076e40b504f9ed07cec2e22fac02262f44e
Vladislav Zorin <zorin.3v6@gmail.com>
2025-04-10 17:02:00 +0300

    Add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """ Менеджер для модели пользователя, с помощью которого создаются обычные пользователи и суперпользователи  """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ Создание обычного пользователя с указанным email и паролем """
+        if not email:
+            raise ValueError('Поле Email должно быть заполнено')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """ Создание суперпользователя с указанным email и паролем """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """ Модель пользователя """
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона', blank=True, null=True)
+    city = models.CharField(max_length=150, verbose_name='Город', blank=True, null=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('id',)
