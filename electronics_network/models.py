4a68cad89933a9b0f48d51996de45abe7268b06c
Vladislav Zorin <zorin.3v6@gmail.com>
2025-04-10 17:06:39 +0300

    Add network models

--- electronics_network/models.py
+++ electronics_network/models.py
@@ -1,3 +1,97 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class SupplyChainMember(models.Model):
+    """ Модель участника цепочки поставки """
+
+    FACTORY = 'factory'
+    RETAIL = 'retail'
+    ENTREPRENEUR = 'entrepreneur'
+
+    TYPE_CHOICES = [
+        (FACTORY, 'Завод'),
+        (RETAIL, 'Розничная сеть'),
+        (ENTREPRENEUR, 'Индивидуальный предприниматель'),
+    ]
+
+    name = models.CharField(max_length=254, unique=True, verbose_name='Название участника сети продаж')
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES, verbose_name='Тип участника')
+    contact = models.OneToOneField(
+        'ContactInfo',
+        on_delete=models.CASCADE,
+        related_name='supply_chain_member',
+        verbose_name='Контактные данные',
+    )
+    supplier = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='supply_chain_members',
+        verbose_name='Поставщик'
+    )
+    debt = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, verbose_name='Задолженность'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='supply_chain_members', verbose_name='Создатель'
+    )
+
+    @property
+    def level(self):
+        """ Вычисление уровня участника в иерархии поставок """
+        lvl = 0
+        supplier = self.supplier
+        while supplier:
+            lvl += 1
+            supplier = supplier.supplier
+        return lvl
+
+    def __str__(self):
+        return f'{self.name} (уровень {self.level} – {self.get_type_display()})'
+
+    class Meta:
+        verbose_name = 'участник цепочки поставок'
+        verbose_name_plural = 'участники цепочки поставок'
+        ordering = ('id',)
+
+
+class ContactInfo(models.Model):
+    """ Модель контактных данных """
+
+    email = models.EmailField(verbose_name='Email')
+    country = models.CharField(max_length=99, verbose_name='Страна')
+    city = models.CharField(max_length=99, verbose_name='Город')
+    street = models.CharField(max_length=99, verbose_name='Улица')
+    house_number = models.CharField(max_length=9, verbose_name='Номер дома')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'контактные данные'
+        verbose_name_plural = 'контактные данные'
+        ordering = ('id',)
+
+
+class Product(models.Model):
+    """ Модель продукта """
+
+    name = models.CharField(max_length=254, verbose_name='Название')
+    model = models.CharField(max_length=254, blank=True, null=True, verbose_name='Модель')
+    release_date = models.DateField(verbose_name='Дата выхода на рынок')
+    members = models.ManyToManyField(
+        'SupplyChainMember',
+        related_name='products',
+        verbose_name='Участники сети'
+    )
+
+    def __str__(self):
+        return f'{self.name} {self.model}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('id',)
